1f1088d2904e0bb5a43dad9aeaf9a5368680fc95
elshaddd <elshadisaev48@gmail.com>
2023-10-24 00:24:05 +0300

    head

--- Util/DisplayAndLeveling.h
+++ Util/DisplayAndLeveling.h
@@ -1,3 +1,6 @@
+#ifndef DISPLAY_AND_LEVEL_H
+#define DISPLAY_AND_LEVEL_H
+
 #include "../Control/PlayerController.h"
 #include "../Generators/FieldGenerator.h"
 
@@ -17,46 +20,6 @@ void leveling(GameField &gameField, PlayerController &controller, int &level)
     }
     controller.setCoordinates({1, 1});
 }
-
-// void display(GameField &gameField, PlayerController &controller, int &level)
-// {
-//     for (int i = 0; i < gameField.getHeight(); i++)
-//     {
-//         for (int j = 0; j < gameField.getWidth(); j++)
-//         {
-//             if (controller.getCoordinates().first == j && controller.getCoordinates().second == i)
-//             {
-//                 std::cout << "p" << ' ';
-//                 if (controller.getCoordinates() == gameField.getExit())
-//                 {
-//                     leveling(gameField, controller, ++level);
-//                 }
-//             }
-//             else if (std::make_pair(j, i) == gameField.getEntrance())
-//             {
-//                 std::cout << "]" << ' ';
-//             }
-//             else if (std::make_pair(j, i) == gameField.getExit())
-//             {
-//                 std::cout << "[" << ' ';
-//             }
-//             else if (gameField.getCell(std::make_pair(j, i)).getEvent() != nullptr)
-//             {
-//                 std::cout << "?" << ' ';
-//             }
-//             else if (gameField.getCell(std::make_pair(j, i)).isPassable())
-//             {
-//                 std::cout << " " << ' ';
-//             }
-
-//             else
-//             {
-//                 std::cout << "*" << ' ';
-//             }
-//         }
-//         std::cout << "\n";
-//     }
-// }
 
 void display(GameField &gameField, PlayerController &controller, int &level)
 {
@@ -99,3 +62,5 @@ void display(GameField &gameField, PlayerController &controller, int &level)
     }
     std::cout << buffer;
 }
+
+#endif
